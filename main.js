98704d53cc27f965e8524ba21cbfa80bf8533bd1
John Philip Mahinay <148014943+jpmahinay@users.noreply.github.com>
2024-02-07 22:55:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,50 +10,56 @@ function getDATAPROJECT() {
 }
 
 function continueScript(projectId, sheetName) {
-  // Show processing message
-  var htmlOutput = HtmlService.createHtmlOutput('<p>Processing.. Please Wait</p>')
-      .setWidth(200)
-      .setHeight(40);
-  HtmlService.createHtmlOutput('<textarea rows="10" cols="50">' + projectId + ', ' + sheetName + '</textarea><br><br><button onclick="google.script.host.close()">Close</button>')
-      .setWidth(400)
-      .setHeight(250);
+  try {
+    // Show processing message
+    var htmlOutput = HtmlService.createHtmlOutput('<p>Processing.. Please Wait</p>')
+        .setWidth(200)
+        .setHeight(40);
+    google.script.host.showDialog(htmlOutput);
 
-  // Get credentials from cache or set them if not cached
-  var cache = CacheService.getScriptCache();
-  var consumerKey = cache.get("consumerKey") || "dcf0884b-bb81-490a-8709-efab05170b1d";
-  var consumerSecret = cache.get("consumerSecret") || "w7FFlxlbtctZUFtEyW7ePTVkXAUlDb3qLKAUVLUJgenq7dVt15Tcb9R-YqLn0Em8";
-  cache.putAll({"consumerKey": consumerKey, "consumerSecret": consumerSecret});
+    // Get credentials from cache or set them if not cached
+    var cache = CacheService.getScriptCache();
+    var consumerKey = cache.get("consumerKey") || "dcf0884b-bb81-490a-8709-efab05170b1d";
+    var consumerSecret = cache.get("consumerSecret") || "w7FFlxlbtctZUFtEyW7ePTVkXAUlDb3qLKAUVLUJgenq7dVt15Tcb9R-YqLn0Em8";
+    cache.putAll({"consumerKey": consumerKey, "consumerSecret": consumerSecret});
 
-  // OAuth Signature 
-  var signature = encodeURIComponent(consumerSecret) + "&";
+    // OAuth Signature 
+    var signature = encodeURIComponent(consumerSecret) + "&";
 
-  // Get headers from cache or set them if not cached
-  var headers = cache.get("headers") || {
-    "Authorization": getAuthHeader(consumerKey, signature) 
-  };
-  cache.put("headers", headers);
+    // Get headers from cache or set them if not cached
+    var headers = cache.get("headers") || {
+      "Authorization": getAuthHeader(consumerKey, signature) 
+    };
+    cache.put("headers", headers);
 
-  // Options
-  var options = {
-    headers: headers,
-    muteHttpExceptions: true
-  };
+    // Options
+    var options = {
+      headers: headers,
+      muteHttpExceptions: true
+    };
 
-  // Get base URL from cache or set it if not cached
-  var baseUrl = cache.get("baseUrl") || "https://api.buzzstream.com/v1";
-  cache.put("baseUrl", baseUrl);
+    // Get base URL from cache or set it if not cached
+    var baseUrl = cache.get("baseUrl") || "https://api.buzzstream.com/v1";
+    cache.put("baseUrl", baseUrl);
 
-  // Get and write websites data
-  var allWebsites = getAllWebsites(baseUrl, projectId, options);
-  
-  // Convert data to a string
-  var dataString = allWebsites.map(website => website.name).join('\n');
+    // Get and write websites data
+    var allWebsites = getAllWebsites(baseUrl, projectId, options);
+    
+    // Convert data to a string
+    var dataString = allWebsites.map(website => website.name).join('\n');
 
-  // Show the data in a text box on the HTML
-  var htmlOutputWithData = HtmlService.createHtmlOutput('<textarea rows="10" cols="50">' + dataString + '</textarea><br><br><button onclick="google.script.host.close()">Close</button>')
-      .setWidth(400)
-      .setHeight(250);
-  google.script.host.showDialog(htmlOutputWithData, 'Buzzstream Data');
+    // Show the data in a text box on the HTML
+    var htmlOutputWithData = HtmlService.createHtmlOutput('<textarea rows="10" cols="50">' + dataString + '</textarea><br><br><button onclick="google.script.host.close()">Close</button>')
+        .setWidth(400)
+        .setHeight(250);
+    google.script.host.showDialog(htmlOutputWithData, 'Buzzstream Data');
+  } catch (error) {
+    // Display error on HTML
+    var errorOutput = HtmlService.createHtmlOutput('<p style="color: red;">Error: ' + error.message + '</p><br><button onclick="google.script.host.close()">Close</button>')
+        .setWidth(300)
+        .setHeight(100);
+    google.script.host.showDialog(errorOutput, 'Error');
+  }
 }
 
 function getAuthHeader(consumerKey, signature) {
